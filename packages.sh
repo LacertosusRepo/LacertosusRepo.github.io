ba1b8cf191100d13f101d768cfb9badfe624940b
LacertosusRepo <LacertosusThemes@gmail.com>
2015-12-15 05:57:34 +0000

    Themes

--- packages.sh
+++ packages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/screenlock debs
 dpkg-deb -bZgzip projects/customreach debs
+dpkg-deb -bZgzip projects/netflixnchill debs
+dpkg-deb -bZgzip projects/neversummer debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
