6a917963297dad252634b48f0f915bb735ee2868
LacertosusRepo <LacertosusThemes@gmail.com>
2015-12-29 08:19:46 +0000

    Added Tweaks

--- packages.sh
+++ packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/screenlock debs
 dpkg-deb -bZgzip projects/customreach debs
 dpkg-deb -bZgzip projects/netflixnchill debs
 dpkg-deb -bZgzip projects/neversummer debs
+dpkg-deb -bZgzip projects/nosbrotate debs
+dpkg-deb -bZgzip projects/screenlock debs
+dpkg-deb -bZgzip projects/volumelock debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
