78633a0048cd40b951af6831080b74a2b510c269
Lacertosus <LacertosusThemes@gmail.com>
2016-01-25 08:59:42 -0700

    Update downloads.sql

--- depictions/resources/counter/downloads.sql
+++ depictions/resources/counter/downloads.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE IF NOT EXISTS `downloads` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `filename` text,
+    `dldate` datetime DEFAULT NULL,
+    `count` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM
