ca15214f85d54d5471df5a5be51d21b94c2a3660
LacertosusRepo <LacertosusThemes@gmail.com>
2016-01-25 22:12:10 -0700

    Unnecesarry Files

--- depictions/resources/counter/downloads.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `downloads` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `filename` text,
-    `dldate` datetime DEFAULT NULL,
-    `count` int(11) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=MyISAM
