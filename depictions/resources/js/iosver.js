5781b2398f76c979aad58f4e7ac6fec9aaef7860
LacertosusDeus <LacertosusThemes@gmail.com>
2018-02-11 15:09:19 -0700

    Update iosver.js

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -1,4 +1,7 @@
 function iOSversion() {
+	
+	var supportCheck = document.getElementById("supportsIOS".innerHTML;
+	
 	if (/iP(hone|od|ad)/.test(navigator.platform)) {
 		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
 		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
@@ -7,14 +10,15 @@ function iOSversion() {
 		return 0;
 	}
 }
-var supportCheck = document.getElementById("supportsIOS".innerHTML;
+
+//supportCheck = document.getElementById("supportsIOS".innerHTML;
 ver = iOSversion();
 if (ver[0] >= 8) {
 	document.getElementById('version').innerHTML='<p>Your device is supported.</p>';
-}
-if (ver[0] >= 10) {
+} if (ver[0] >= 10) {
 	document.getElementById('version').innerHTML='<p>Your device is supported.</p>';
-}
-else {
+} if (supportCheck = 11) {
+	document.getElementById('version').innerHTML='<p>It Work.</p>';
+} else {
 	document.getElementById('version').innerHTML='<p>Your device is not supported. 3</p>';
 }
