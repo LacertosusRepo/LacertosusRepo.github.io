bf305d860cf56564d7bf9b4c3026e5a757ff7ad5
LacertosusDeus <LacertosusThemes@gmail.com>
2018-02-11 16:20:29 -0700

    Update iosver.js

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -1,4 +1,4 @@
-function iOSversion() {		
+function iOSversion() {	
 	if (/iP(hone|od|ad)/.test(navigator.platform)) {
 		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
 		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
@@ -8,18 +8,23 @@ function iOSversion() {
 	}
 }
 
-ver = iOSversion();
 supportCheck = document.getElementById('supportsIOS').innerHTML;
+ver = iOSversion();
 if (supportCheck = "all") {
+	document getElementById('version').innerHTML='<p>Your device is supported.</p>';
+} 
+if (ver[0] >= 8 && supportCheck = "8") {
 	document.getElementById('version').innerHTML='<p>Your device is supported.</p>';
-} if (ver[0] >= 8 && supportCheck = "8") {
+}
+if (ver[0] >= 9 && supportCheck = "9") {
 	document.getElementById('version').innerHTML='<p>Your device is supported.</p>';
-} if (ver[0] >= 9 && supportCheck = "9") {
+}
+if (ver[0] >= 10 && supportCheck = "10") {
 	document.getElementById('version').innerHTML='<p>Your device is supported.</p>';
-} if (ver[0] >= 10 && supportCheck = "10") {
+}
+if (ver[0] >= 11 && supportCheck = "11") {
 	document.getElementById('version').innerHTML='<p>Your device is supported.</p>';
-} if (supportCheck = "11") {
-	document.getElementById('version').innerHTML='<p>Your device is supported.</p>';
-} else {
+}
+else {
 	document.getElementById('version').innerHTML='<p>Your device is not supported.</p>';
 }
