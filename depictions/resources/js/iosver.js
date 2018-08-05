5fefc2f523fb487a85fc533e1abeef2ffb0320f1
LacertosusDeus <LacertosusThemes@gmail.com>
2018-08-05 12:51:47 -0600

    Update iosver.js

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -1,6 +1,6 @@
 //Thank you Matchstic, you beautiful person
 const VERSION_CHECK_SUPPORTED = "<p>iOS %s is supported. ✓</p>";
-const VERSION_CHECK_UNSUPPORTED = "<p>iOS %s is currently not supported. ✕</p>";
+const VERSION_CHECK_UNSUPPORTED = "<p>iOS %s is not supported. ✕</p>";
 const VERSION_CHECK_UNCONFIRMED = "<p>iOS %s has not been tested!</p>";
 
 function iOSversion() {
@@ -16,7 +16,7 @@ function iOSversion() {
 var maxVersion = document.getElementById('maxVersion').innerHTML;
 var minVersion = document.getElementById('minVersion').innerHTML;
 var version = iOSversion();
-var versionNumber = parseFloat(version[0] + "." + version[1] + "." + version[2]);
+var versionNumber = parseFloat(version[0] + "." + version[1]);
 	
 	if(versionNumber >= minVersion && version <= maxVersion) {
 		document.getElementById('compatibility').innerHTML=VERSION_CHECK_SUPPORTED.replace("%s", versionNumber);
