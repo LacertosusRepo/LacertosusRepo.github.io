7de2c8d512a9b7ebc5f6be1c313d6ef2a794ffe2
LacertosusDeus <LacertosusThemes@gmail.com>
2019-02-16 13:28:10 -0700

    Fixes "undefined" showing as supported.

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -1,5 +1,6 @@
 //Thank you Matchstic, you beautiful person
 const VERSION_CHECK_SUPPORTED = "<p>iOS %s is supported. ✓</p>";
+const VERSION_CHECK_INFO = "<p>Compatible with iOS %n to %x.";
 const VERSION_CHECK_UNSUPPORTED = "<p>iOS %s is not supported. ✕</p>";
 const VERSION_CHECK_UNCONFIRMED = "<p>iOS %s has not been tested!</p>";
 
@@ -7,8 +8,7 @@ function iOSversion() {
 	if (/iP(hone|od|ad)/.test(navigator.platform)) {
 		var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
 		return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-	}
-	else {
+	}	else {
 		return 0;
 	}
 }
@@ -17,17 +17,17 @@ var maxVersion = document.getElementById('maxVersion').innerHTML;
 var minVersion = document.getElementById('minVersion').innerHTML;
 var version = iOSversion();
 var versionNumber = String(version[0] + "." + version[1]);
-	
-	if(versionNumber >= minVersion && version <= maxVersion) {
-		document.getElementById('compatibility').innerHTML=VERSION_CHECK_SUPPORTED.replace("%s", versionNumber);
+  
+  if(!version) {
+  	document.getElementById('compatibility').innerHTML=VERSION_CHECK_INFO.replace("%n", minVersion).replace("%x", maxVersion);
+		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+  } else if(versionNumber >= minVersion && version <= maxVersion) {
+  	document.getElementById('compatibility').innerHTML=VERSION_CHECK_SUPPORTED.replace("%s", versionNumber);
 		document.body.style.setProperty("--title-color", "rgba(39,174,96, 0.5)");
-	} else if(versionNumber > maxVersion) {
-		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNCONFIRMED.replace("%s", versionNumber);
+  } else if(versionNumber > maxVersion) {
+  	document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNCONFIRMED.replace("%s", versionNumber);
 		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
-	} else if(!versionNumber) {
-		document.getElementById('compatibility').innerHTML="<p>Can't get device version.</p>";
+  } else {
+  	document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNSUPPORTED.replace("%s", versionNumber);
 		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
-	} else {
-		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNSUPPORTED.replace("%s", versionNumber);
-		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
-	}
+  }
