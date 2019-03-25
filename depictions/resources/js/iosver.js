3a4c165fa37cf9c68a11e5f71b0d5db8cd93f7aa
Lacertosus <LacertosusThemes@gmail.com>
2019-03-25 13:53:37 -0600

    Version Check Update

--- depictions/resources/js/iosver.js
+++ depictions/resources/js/iosver.js
@@ -1,6 +1,6 @@
 //Thank you Matchstic, you beautiful person
 const VERSION_CHECK_SUPPORTED = "<p>iOS %s is supported. ✓</p>";
-const VERSION_CHECK_INFO = "<p>Compatible with iOS %n to %x.";
+const VERSION_CHECK_INFO = "<p>Compatible with iOS %n to %x. Add this repo to Cydia to install.";
 const VERSION_CHECK_UNSUPPORTED = "<p>iOS %s is not supported. ✕</p>";
 const VERSION_CHECK_BETA = "<p>This is beta software! iOS %s may not be supported.</p>";
 
@@ -19,8 +19,13 @@ var version = iOSversion();
 var versionNumber = String(version[0] + "." + version[1]);
 
 	if(!version) {
-		document.getElementById('compatibility').innerHTML=VERSION_CHECK_INFO.replace("%n", minVersion).replace("%x", maxVersion);
-		document.body.style.setProperty("--title-color", "rgba(39,174,96, 0.5)");
+		if(minVersion == "beta" || maxVersion == "beta") {
+			document.getElementById('compatibility').innerHTML="<p>This is beta software! It may not work on certain devices.</p>";
+			document.body.style.setProperty("--title-color", "rgba(225,174,66, 0.5)");
+		} else {
+			document.getElementById('compatibility').innerHTML=VERSION_CHECK_INFO.replace("%n", minVersion).replace("%x", maxVersion);
+			document.body.style.setProperty("--title-color", "rgba(39,174,96, 0.5)");
+		}
 	} else if(versionNumber >= minVersion && version <= maxVersion) {
 		document.getElementById('compatibility').innerHTML=VERSION_CHECK_SUPPORTED.replace("%s", versionNumber);
 		document.body.style.setProperty("--title-color", "rgba(39,174,96, 0.5)");
@@ -29,7 +34,7 @@ var versionNumber = String(version[0] + "." + version[1]);
 		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
 	} else if(minVersion == "beta" || maxVersion == "beta") {
 		document.getElementById('compatibility').innerHTML=VERSION_CHECK_BETA.replace("%s", versionNumber);
-		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
+		document.body.style.setProperty("--title-color", "rgba(225,174,66, 0.5)");
 	} else {
 		document.getElementById('compatibility').innerHTML=VERSION_CHECK_UNSUPPORTED.replace("%s", versionNumber);
 		document.body.style.setProperty("--title-color", "rgba(192,57,43, 0.5)");
